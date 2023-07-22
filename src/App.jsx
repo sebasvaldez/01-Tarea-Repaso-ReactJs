79b7e79b48331035fb11fe6f639ff754ea0e1477
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-07-22 12:31:08 -0300

    Se agrego el Novedades.json

--- src/App.jsx
+++ src/App.jsx
@@ -68,25 +68,23 @@ const App = () => {
           <div className="feature col text-white">
             <h3 className="fs-2">Featured title</h3>
             <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="icon-link">
+            <a href="#" className="text-warning">
               Call to action
-              <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
             </a>
           </div>
           <div className="feature col text-white">
             <h3 className="fs-2">Featured title</h3>
             <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="icon-link">
+            <a href="#" className="text-warning">
               Call to action
-              <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
             </a>
           </div>
           <div className="feature col text-white">
             <h3 className="fs-2">Featured title</h3>
             <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-            <a href="#" className="icon-link">
+            <a href="#" className="text-warning">
               Call to action
-              <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
+
             </a>
           </div>
         </div>
