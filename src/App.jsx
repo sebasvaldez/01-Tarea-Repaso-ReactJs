0aad1aa404afde02e22f59ecdde1fb4d4cb6cc57
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-07-22 12:19:35 -0300

    Se agrego el features a l template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './styles/index.css';
-import logo from './assets/logo.png';
+import './styles/features.css';
 
 const App = () => {
  
@@ -9,9 +9,6 @@ const App = () => {
     <>
       <nav className="navbar navbar-expand-lg bg-dark" >
         <div className="container-fluid">
-          <a className="navbar-brand text-white" href="#">
-           <img src={ logo } width={40} />
-          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -56,7 +53,7 @@ const App = () => {
         </div>
       </nav>
       <box>
-        <div className='bg-banner'>
+        <div className='bg-white p-5 text-center'>
           <div style={{fontSize: '80px', fontWeight: 'bold'}}>WEEK SALE!</div>
           <div style={{fontSize: '30px'}}>Las mejores ofertas en celulares!</div>
           <div style={{ marginTop: '20px'}}>
@@ -64,6 +61,36 @@ const App = () => {
           </div>
         </div>
       </box>
+
+      <div className="container px-4 py-5" id="featured-3">
+        <h2 className="pb-2 border-bottom text-white">NOVEDADES</h2>
+        <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          <div className="feature col text-white">
+            <h3 className="fs-2">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+            <a href="#" className="icon-link">
+              Call to action
+              <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
+            </a>
+          </div>
+          <div className="feature col text-white">
+            <h3 className="fs-2">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+            <a href="#" className="icon-link">
+              Call to action
+              <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
+            </a>
+          </div>
+          <div className="feature col text-white">
+            <h3 className="fs-2">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+            <a href="#" className="icon-link">
+              Call to action
+              <svg className="bi"><use xlink:href="#chevron-right"></use></svg>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
