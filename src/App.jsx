a3c5083c8acd02a55d07bd91bd05d0ccf67b56ab
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-07-24 09:12:41 -0300

    Se modifico el nombre de los links

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,17 @@ const App = () => {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item ">
                 <a className="nav-link active text-white" aria-current="page" href="#">
-                  Home
+                  Link-1
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link active text-white" aria-current="page" href="#">
-                  Productos
+                  Link-2
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link active text-white" aria-current="page" href="#">
-                  Contacto
+                  Link-3
                 </a>
               </li>
             </ul>
