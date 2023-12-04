18e5b25f189a5ded0bb26d3d9155a08b5ffba6c4
Marcelo Sebastián Valdez <48698148+sebasvaldez@users.noreply.github.com>
2023-12-04 14:01:24 -0300

    correccion de la imagen hero

--- src/components/banners/CustomBanner.jsx
+++ src/components/banners/CustomBanner.jsx
@@ -13,8 +13,8 @@ export const CustomBanner = () => {
           <div style={{ fontSize: "30px" }}>Curso de React Avanzado!</div>
           <button className="btn btn-primary btn-sm btn-md-lg mt-4 mb-4">VER MAS</button>
         </div>
-        <div className="col-5  d-md-flex">
-          <img src={hero} alt="header image" width={200} />
+        <div className="col-5  d-md-flex hero-image">
+          <img src={hero} alt="header image" />
         </div>
       </div>
     </div>
